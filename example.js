f99c78a23d60d58735da6223a4a85e269f7a371a
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-02-02 16:14:27 +0000

    Working fetch request

---
+++ example.js
@@ -0,0 +1,31 @@
+const API_KEY = "3ed390c11a1c301b784fbf0a4cb2859465e6f24e";
+
+const getHolidays = () => {
+  //fetch
+  fetch(
+    `https://calendarific.com/api/v2/holidays?&api_key=${API_KEY}&country=GB&year=2021`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.response.holidays);
+      const holidays = data.response.holidays;
+      holidays.forEach((holiday) => {
+        const day = holiday.date.datetime.day;
+        const month = holiday.date.datetime.month;
+        const year = holiday.date.datetime.year;
+        // console.log(holiday.name);
+        const eachName = holiday.name;
+
+        const holName = document.createElement("h1");
+        holName.innerHTML = `Holiday: ${eachName}`;
+        holName.classList.add("red");
+        document.body.appendChild(holName);
+
+        const newDate = document.createElement("h1");
+        newDate.innerHTML = `Date: ${day}.${month}.${year}`;
+        newDate.classList.add("blue");
+        document.body.appendChild(newDate);
+      });
+    });
+};
+getHolidays();
